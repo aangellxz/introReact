05bf13a079a4ce54ae781abfdb44a31ee7f946c2
aangellxz <aangellxz2008@gmail.com>
2025-05-26 22:19:00 -0300

    feat: Introdução sobre React

---
+++ ola/src/componets/Body.jsx
@@ -0,0 +1,12 @@
+import './Body.css'
+
+function Body() {
+    return (
+        <div className='Body'>
+            <h2 className='titulo2'>Dinos são legais</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique mollitia corporis voluptatibus maxime quos nihil ut porro adipisci illum odit culpa reiciendis nisi beatae itaque, in quisquam ex modi temporibus!</p>
+        </div>
+    )
+}
+
+export default Body
